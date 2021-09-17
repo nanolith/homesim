fb1fea0e9f9e35fc3e625dc82b6311f34aa8291c
Justin Handville <nanolith@gmail.com>
2021-09-17 03:30:07 -0400

    Added propagation delay tests.

--- test/test_ic7432.cpp
+++ test/test_ic7432.cpp
@@ -204,3 +204,82 @@ TEST(gate4)
     propagate();
     TEST_EXPECT(out.get_signal() == true);
 }
+
+/**
+ * Verify that the correct propagation delay is set by default.
+ */
+TEST(propagation_time_default)
+{
+    /* force the global agenda into a known state. */
+    global_agenda.clear();
+
+    /* the default IC propagation delay is 27 nanoseconds. */
+    TEST_EXPECT(ic7432_delay == 22.0 * nanoseconds_to_seconds_scale);
+
+    /* the global agenda starts at t = 0. */
+    TEST_ASSERT(global_agenda.current_time() == 0.0);
+
+    /* create 7432 ic. */
+    wire lhs;
+    wire rhs;
+    wire out;
+    wire dummy_lhs1;
+    wire dummy_lhs2;
+    wire dummy_lhs3;
+    wire dummy_rhs1;
+    wire dummy_rhs2;
+    wire dummy_rhs3;
+    wire dummy_out1;
+    wire dummy_out2;
+    wire dummy_out3;
+    ic7432 ic(
+        &lhs, &rhs, &out, &dummy_lhs1, &dummy_rhs1, &dummy_out1,
+        &dummy_out2, &dummy_lhs2, &dummy_rhs2, &dummy_out3, &dummy_lhs3,
+        &dummy_rhs3);
+
+    /* propagate the initial signal for the ic. */
+    propagate();
+
+    /* verify that current time according to the global agenda is our
+     * propagation delay. */
+    TEST_ASSERT(global_agenda.current_time() == ic7432_delay);
+}
+
+/**
+ * Verify that the propagation delay can be overridden in the constructor.
+ */
+TEST(propagation_time_override)
+{
+    double EXPECTED_PROPAGATION_DELAY = 44.0 * nanoseconds_to_seconds_scale;
+
+    /* force the global agenda into a known state. */
+    global_agenda.clear();
+
+    /* the global agenda starts at t = 0. */
+    TEST_ASSERT(global_agenda.current_time() == 0.0);
+
+    /* create 7432 ic. */
+    wire lhs;
+    wire rhs;
+    wire out;
+    wire dummy_lhs1;
+    wire dummy_lhs2;
+    wire dummy_lhs3;
+    wire dummy_rhs1;
+    wire dummy_rhs2;
+    wire dummy_rhs3;
+    wire dummy_out1;
+    wire dummy_out2;
+    wire dummy_out3;
+    ic7432 ic(
+        &lhs, &rhs, &out, &dummy_lhs1, &dummy_rhs1, &dummy_out1,
+        &dummy_out2, &dummy_lhs2, &dummy_rhs2, &dummy_out3, &dummy_lhs3,
+        &dummy_rhs3, EXPECTED_PROPAGATION_DELAY);
+
+    /* propagate the initial signal for the ic. */
+    propagate();
+
+    /* verify that current time according to the global agenda is our
+     * propagation delay. */
+    TEST_ASSERT(global_agenda.current_time() == EXPECTED_PROPAGATION_DELAY);
+}
